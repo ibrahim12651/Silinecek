6b3c27804603acbc00f9858e7f0c1e485bb01223
İbrahim Bektaş <39805679+ibrahim12651@users.noreply.github.com>
2023-04-05 14:57:43 +0300

    Update tablo.php

--- tablo.php
+++ tablo.php
@@ -1,36 +1,36 @@
 <?php
 
+
 include("ayar.php");
 
-$sorgu = "SELECT * FROM malzemeler";
-$sorgu = "UPDATE test_tablo SET email=‘ZZZZ@mail.com’ WHERE id=1";
-$sorgu =  "DELETE FROM test_tablo WHERE id=1 ";
+$sorgu = "SELECT * FROM urunler";
 
-
-if($veri= mysqli_query($baglanti,$sorgu)){
+if($veri=mysqli_query($baglanti,$sorgu)){
     if(mysqli_num_rows($veri) > 0){
-        echo "<table>";
+        echo "<table border='1'>";
             echo "<tr>";
-                echo "<th>Id</th>";
-                echo "<th>Ad</th>";
-                echo "<th>Soyad</th>";
-                echo "<th>Email</th>";
+                echo "<th>URUN_ID</th>";
+                echo "<th>URUN_ADI</th>";
+                echo "<th>MARKASI</th>";
+                echo "<th>MODELI</th>";
+                echo "<th>FIYATI</th>";
+                echo "<th>STOK</th>";
             echo "</tr>";
     }
-    while ($kayit = mysqli_fetch_array($veri)){
+    while($kayit = mysqli_fetch_array($veri)){
         echo "<tr>";
-            echo "<td>". $kayit['id'] . "</td>";
-            echo "<td>". $kayit['Markasi'] . "</td>" ;
-            echo "<td>". $kayit['Urun_adi'] . "</td>" ;
-            echo "<td>". $kayit['Stok']. "</td>";
+            echo "<td>".$kayit["URUN_ID"]."</td>"; 
+            echo "<td>".$kayit["URUN_ADI"]."</td>";
+            echo "<td>".$kayit["MARKASI"]."</td>";
+            echo "<td>".$kayit["MODELI"]."</td>";
+            echo "<td>".$kayit["FIYATI"]."</td>";
+            echo "<td>".$kayit["STOK"]."</td>";
         echo "</tr>";
     }
-    echo "</table>";
     mysqli_free_result($veri);
-} else {
-    echo "Kayıt Bulunamadı";
+}else{
+    echo "Bağlantı Olumlu değil..";
 }
-
 mysqli_close($baglanti);
 
 
